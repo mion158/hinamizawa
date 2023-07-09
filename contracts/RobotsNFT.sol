7d2fb0b03b4d8405850c53d2bf26f56e8d71488c
mion158 <111487792+mion158@users.noreply.github.com>
2023-07-09 14:27:23 +0100

    basic mint function

--- contracts/RobotsNFT.sol
+++ contracts/RobotsNFT.sol
@@ -5,3 +5,24 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+contract RobotsNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("RobotsNFT","BR") {
+
+    }
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
